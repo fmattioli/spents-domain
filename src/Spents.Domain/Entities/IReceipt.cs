f4418613d53137cc74d2a6db6e09602cab745793
ext.felipe.mattioli <ext.felipe.mattioli@farfetch.com>
2023-02-02 11:10:37 +0000

    First commit

---
+++ src/Spents.Domain/Entities/IReceipt.cs
@@ -0,0 +1,12 @@
+﻿using Spents.Domain.ValueObjects;
+
+namespace Spents.Domain.Entities
+{
+    public interface IReceipt
+    {
+        public Guid Id { get; set; }
+        public string ReceiptName { get; set; }
+        public DateTime ReceiptDate { get; set; }
+        public IEnumerable<IReceiptItem> ReceiptItems { get; set; }
+    }
+}
