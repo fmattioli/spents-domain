bf9fd380b3b6aadf5a77da5ef9db28041b4ec53a
Felipe <felipe-mattioli98@hotmail.com>
2023-02-04 11:04:25 +0000

    FEAT: Changing properties logic

---
+++ src/Spents.Domain/Entities/Receipt.cs
@@ -0,0 +1,12 @@
+﻿using Spents.Domain.ValueObjects;
+
+namespace Spents.Domain.Entities
+{
+    public abstract class Receipt
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string ReceiptName { get; set; } = null!;
+        public virtual DateTime ReceiptDate { get; set; }
+        public virtual IEnumerable<ReceiptItem> ReceiptItems { get; set; } = new List<ReceiptItem>();
+    }
+}
---
+++ src/Spents.Domain/ValueObjects/ReceiptItem.cs
@@ -0,0 +1,17 @@
+﻿namespace Spents.Domain.ValueObjects
+{
+    public abstract class ReceiptItem
+    {
+        public virtual Guid Id { get; set; }
+        public virtual string Name { get; set; } = null!;
+        public virtual short Quantity { get; set; }
+        public virtual decimal ItemPrice { get; set; }
+        public virtual string Observation { get; set; } = null!;
+        public virtual decimal TotalPrice { get { return ItemPrice * Quantity; } }
+
+        public virtual decimal CalculateTotalPrice(decimal itemPrice, short quantity)
+        {
+            return itemPrice * quantity;
+        }
+    }
+}
