b1723c6dc8bce4947912e1b0bd595d082712f26e
Felipe <felipe-mattioli98@hotmail.com>
2023-02-04 13:13:59 +0000

    FEAT: Changing logic

--- src/Spents.Domain/Entities/Receipt.cs
+++ src/Spents.Domain/Entities/Receipt.cs
@@ -2,11 +2,11 @@
 
 namespace Spents.Domain.Entities
 {
-    public abstract class Receipt
+    public class Receipt
     {
-        public virtual Guid Id { get; set; }
-        public virtual string ReceiptName { get; set; } = null!;
-        public virtual DateTime ReceiptDate { get; set; }
-        public virtual IEnumerable<ReceiptItem> ReceiptItems { get; set; } = new List<ReceiptItem>();
+        public Guid Id { get; set; }
+        public string ReceiptName { get; set; } = null!;
+        public DateTime ReceiptDate { get; set; }
+        public IEnumerable<ReceiptItem> ReceiptItems { get; set; } = new List<ReceiptItem>();
     }
 }
--- src/Spents.Domain/ValueObjects/ReceiptItem.cs
+++ src/Spents.Domain/ValueObjects/ReceiptItem.cs
@@ -1,26 +1,12 @@
 ﻿namespace Spents.Domain.ValueObjects
 {
-    public abstract class ReceiptItem
+    public record ReceiptItem
     {
-        protected ReceiptItem(Guid id, string name, short quantity, decimal itemPrice, string observation)
-        {
-            Id = id;
-            Name = name;
-            Quantity = quantity;
-            ItemPrice = itemPrice;
-            Observation = observation;
-        }
-
         public virtual Guid Id { get; set; }
         public virtual string Name { get; set; } = null!;
         public virtual short Quantity { get; set; }
         public virtual decimal ItemPrice { get; set; }
         public virtual string Observation { get; set; } = null!;
         public virtual decimal TotalPrice { get { return ItemPrice * Quantity; } }
-
-        public virtual decimal CalculateTotalPrice(decimal itemPrice, short quantity)
-        {
-            return itemPrice * quantity;
-        }
     }
 }
